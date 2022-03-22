2cb3420cc7da28479c89a38784b7d1ffd8a231da
Ben Ho <bennyhch@gmail.com>
2022-03-22 12:42:49 +0000

    filter bar added, more stylign after adding the movie poster

--- my-app/src/components/Home/home.styled.ts
+++ my-app/src/components/Home/home.styled.ts
@@ -1,7 +1,46 @@
 import styled from 'styled-components';
 
 const StyledHome = styled.div`
-  color: blue;
+  color: white;
+
+  .filter {
+    text-align: center;
+  }
+
+  .filter input {
+    border: 1px solid #6f6f6f;
+    color: #6f6f6f;
+    background-color: var(--bg-color);
+    border-radius: 25px;
+    width: 35%;
+    padding: 25px;
+    margin-top: 50px;
+  }
+
+  .filter input: focus {
+    border: 1px solid var(--light-purple);
+    transition: 1s;
+    outline: none;
+  }
+
+  .row {
+    margin: 20px;
+  }
+  
+  .heading {
+    padding-top: 15px;
+    padding-bottom: 15px;
+    font-weight: 500;
+    font-size: 32px;
+  }
+
+  .on-the-go {
+    border: 2px solid yellow;
+  }
+
+  .completed {
+    border: 2px solid red;
+  }
 `;
 
 export default StyledHome;
