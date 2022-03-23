e8c2f1bcd49f82ab7541df59cb97e8262fd654a5
Ben Ho <bennyhch@gmail.com>
2022-03-23 16:02:41 +0000

    replacing show button with  accordion

--- my-app/src/components/Forum/forumReplies.styled.ts
+++ my-app/src/components/Forum/forumReplies.styled.ts
@@ -1,21 +1,21 @@
 import styled from 'styled-components';
 
 const StyledForumReplies = styled.div`
-// border: 2px solid red; 
+border: 2px solid red; 
 display: flex;
-justify-content: flex-end;
+justify-content: space-between;
 margin-top: 5px;
 
 
 .reply-modal {
-  // border: 2px solid blue;
+  border: 2px solid blue;
   text-align: right;
 }
 
 .replies {
-  // border: 5px solid yellow;
-  z-index: 1;
-  height: 50px;
+  border: 5px solid yellow;
+  // z-index: 1;
+  // height: 50px;
   
   
 }
